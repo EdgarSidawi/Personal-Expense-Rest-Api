75d21456d0c428b39f672eb932cc762792e851e2
Edgar Sidawi <romantibuai@gmail.com>
2020-01-24 17:17:25 +0000

    session model factory

--- database/factories/SessionModelFactory.php
+++ database/factories/SessionModelFactory.php
@@ -7,6 +7,14 @@ use Faker\Generator as Faker;
 
 $factory->define(SessionModel::class, function (Faker $faker) {
     return [
-        //
+        'bugbet' => $faker->doubleval, //$faker->numberBetween($min = 1, $max = 99),
+        'completed' => false,
+        'startDate' => $faker->date,
+        'endDate' => $faker->dateTimeBetween('+1 week', '+1 month'),
+        'user_id' => function () {
+            return  factory(User::class)->create()->id;
+            // return  User::all()->random();
+        }
+
     ];
 });
