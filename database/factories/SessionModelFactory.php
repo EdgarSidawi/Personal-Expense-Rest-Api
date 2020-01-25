73f7302f83806c48c5defb59d775bbb4df4c9e21
Edgar Sidawi <romantibuai@gmail.com>
2020-01-25 11:45:11 +0000

    Session model factory updated

--- database/factories/SessionModelFactory.php
+++ database/factories/SessionModelFactory.php
@@ -7,11 +7,12 @@ use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(SessionModel::class, function (Faker $faker) {
+    // $currentDate = Carbon\Carbon::now();
     return [
         'bugbet' => $faker->numberBetween($min = 1, $max = 999),
         'completed' => false,
-        'startDate' => $faker->date,
-        'endDate' => $faker->dateTimeBetween('+1 week', '+1 month'),
+        'startDate' => Carbon\Carbon::now(),
+        'endDate' => Carbon\Carbon::now()->addDays(30),
         'user_id' => function () {
             // return  factory(User::class)->create()->id;
             return  User::all()->random();
