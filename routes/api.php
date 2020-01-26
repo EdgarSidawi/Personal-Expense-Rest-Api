f6b0357a31d99bab4f98645a69e4a5e38ed4afdc
Edgar Sidawi <romantibuai@gmail.com>
2020-01-26 18:59:09 +0000

    set password attributes for user model

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 Route::group([
 
+    'middleware' => 'api',
     'prefix' => 'auth'
 
-], function () {
+], function ($router) {
     Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
